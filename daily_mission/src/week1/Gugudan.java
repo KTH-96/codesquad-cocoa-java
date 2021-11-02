43f6b2d28b820b54bee740dfbac4c2d50fe8a2fe
KTH-96 <taehyun960@gmail.com>
2021-11-02 14:21:42 +0900

    refactor:구구단 메서드사용

--- daily_mission/src/week1/Gugudan.java
+++ daily_mission/src/week1/Gugudan.java
@@ -1,19 +1,53 @@
 package week1;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Gugudan {
-    public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("시작숫자와 마지막 숫자 입력: ");
-        int startNum = sc.nextInt();
-        int endNum = sc.nextInt();
-        for (int i = startNum; i <= endNum; i++) {
-            for (int j = 1; j <= 9; j++) {
-                sb.append(i + " * " + j + " = " + i * j).append('\n');
+    static int startNum,endNum;
+    static StringBuilder sb = new StringBuilder();
+
+    public boolean writeNum() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("시작단 과 끝단을 입력(1 ~10)");
+        startNum = Integer.parseInt(br.readLine());
+        endNum = Integer.parseInt(br.readLine());
+        if (startNum < 1 || endNum > 10 || startNum > endNum) {
+            System.out.println("다시");
+            return false;
+        }
+        br.close();
+        return true;
+    }
+
+    public static void getGugudan() {
+        for (int i = startNum; i < endNum + 1; i++) {
+            for (int j = 1; j < 10; j++) {
+                sb.append(i + " * "+ j +" = "+ (i*j)).append('\n');
             }
         }
         System.out.println(sb);
+    }
+
+    @Override
+    public String toString() {
+        return "Gugudan{"+
+                "start = "+ startNum+
+                ", end = "+endNum+
+                '}';
+    }
+
+    public static void main(String[] args) throws IOException {
+        Gugudan gugudan = new Gugudan();
+
+        boolean valid = gugudan.writeNum();
+
+        while (!valid){
+            valid = gugudan.writeNum();
+        }
+
+        System.out.println(gugudan);
+        getGugudan();
     }
 }
