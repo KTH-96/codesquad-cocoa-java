3f172f9d8a494541cd93085bc878cc90411534a8
KTH-96 <taehyun960@gmail.com>
2021-11-17 17:23:15 +0900

    한글 시계 생성후 쉘명령어 추가

---
+++ daily_mission/src/week3/shell/hclock/time/Min.java
@@ -0,0 +1,42 @@
+package week3.shell.hclock.time;
+
+public enum Min {
+    M1(1,"일", 4, 1),
+    M2(2,"이", 4, 2),
+    M3(3,"삼", 4, 3),
+    M4(4,"사", 4, 4),
+    M5(5,"오", 5, 1),
+    M6(6,"육", 4, 5),
+    M7(7,"칠", 5, 2),
+    M8(8,"팔", 5, 3),
+    M9(9,"구", 5, 4),
+    M10(10,"십", 3, 5);
+
+    private final int min;
+    private final String hangulMin;
+    private final int y;
+    private final int x;
+
+    Min(int min, String hangulMin, int y, int x) {
+        this.min = min;
+        this.hangulMin = hangulMin;
+        this.y = y;
+        this.x = x;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public String getHangulMin() {
+        return hangulMin;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
--- daily_mission/src/week3/shell/hclock/time/TimeService.java
+++ daily_mission/src/week3/shell/hclock/time/TimeService.java
@@ -2,33 +2,74 @@ package week3.shell.hclock.time;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class TimeService {
-    public static final String ANSI_RESET = "\u001B[0M";
-    public static final String ANSI_PURPLE = "\u001B[35M";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_PURPLE = "\033[0;32m";
     private Time time;
+    private Hour hour;
+    private Min min;
 
-    private String[][] arr = {{"한","두","셋","넷","다","섯"},
+    public void run() {
+         String[][] arr = {{"한","두","셋","넷","다","섯"},
                         {"여","섯","일","곱","여","덟"},
                         {"아","홉","열","한","두","시"},
                         {"자","이","삼","사","오","십"},
                         {"정","일","이","삼","사","육"},
                         {"오","오","칠","팔","구","분"}};
-
-    public void run() {
         getHourAndMin();
-        changeColorHour();
-        changeColorMin();
+        if (time.getHour() % 12 != 0) {
+            changeColorHour(arr, time.getHour(), hour.values());
+            changeColorMin(arr, time.getMin(), min.values());
+        }else{
+            changeColor(arr);
+        }
         showClock(arr);
     }
-    //todo:해당시간의 좌표값에 색깔을 넣기, 12 넘어가면 다시 초기화, 12정오 00자정
-    private void changeColorHour() {
+
+    private void changeColor(String[][] arr) {
+        if (time.getHour() == 12 && time.getMin() == 0){
+            arr[4][0] = ANSI_PURPLE + "정" + ANSI_RESET;
+            arr[5][0] = ANSI_PURPLE + "오" + ANSI_RESET;
+        }else if (time.getHour() == 0 && time.getMin() == 0){
+            arr[3][0] = ANSI_PURPLE + "자" + ANSI_RESET;
+            arr[4][0] = ANSI_PURPLE + "정" + ANSI_RESET;
+        }
+    }
+
+    private void changeColorHour(String[][] arr, int hour, Hour[] hangulHour) {
+        int value = hour % 12;
+
+        arr[2][5] = ANSI_PURPLE + "시" + ANSI_RESET;
+        for (Hour hourValue : hangulHour) {
+            if (value == hourValue.getHour()) {
+                arr[hourValue.getY()][hourValue.getX()] = ANSI_PURPLE + hourValue.getHangulHour() + ANSI_RESET;
+            }
+        }
 
     }
-    //todo:해당분의 좌표값에 색깔을 넣기, 십분단위 /10, 일분단위 %10 해서 해볼예정
-    private void changeColorMin() {
+
+    private void changeColorMin(String[][] arr, int min, Min[] hangulMin) {
+        int tenMin = min / 10;
+        int oneMin = min % 10;
+        if (min != 0){
+            arr[5][5] = ANSI_PURPLE + "분" + ANSI_RESET;
+        }
+        for (Min minValue : hangulMin) {
+            if (tenMin > 0){
+                arr[3][5] = ANSI_PURPLE + "십" + ANSI_RESET;
+                if (oneMin == minValue.getMin()){
+                    arr[minValue.getY()][minValue.getX()] = ANSI_PURPLE + minValue.getHangulMin() + ANSI_RESET;
+                }
+            }else {
+                if (oneMin == minValue.getMin()){
+                    arr[minValue.getY()][minValue.getX()] = ANSI_PURPLE + minValue.getHangulMin() + ANSI_RESET;
+                }
+            }
+        }
     }
 
     private void getHourAndMin() {
@@ -36,7 +77,6 @@ public class TimeService {
         String formattedNow = now.format(DateTimeFormatter.ofPattern("HH/mm"));
         StringTokenizer st = new StringTokenizer(formattedNow, "/");
         time = new Time(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-        System.out.println(formattedNow);
     }
 
     private void showClock(String[][] arr) {
