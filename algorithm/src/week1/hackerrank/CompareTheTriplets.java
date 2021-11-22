75464b53b9481a28cfeb61a7fdb3e270d1a318ec
KTH-96 <taehyun960@gmail.com>
2021-11-22 10:20:24 +0900

    알고리즘 형식 변경

--- algorithm/src/week1/hackerrank/CompareTheTriplets.java
+++ algorithm/src/week1/hackerrank/CompareTheTriplets.java
@@ -8,59 +8,26 @@ import java.util.stream.Stream;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-class Result {
 
-    /*
-     * Complete the 'compareTriplets' function below.
-     *
-     * The function is expected to return an INTEGER_ARRAY.
-     * The function accepts following parameters:
-     *  1. INTEGER_ARRAY a
-     *  2. INTEGER_ARRAY b
-     */
-
-    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
-        // Write your code here
-        int aWin = 0;
-        int bWin = 0;
-        for (int i = 0; i < a.size(); i++) {
-            if (a.get(i) > b.get(i)) {
-                aWin++;
-            }else if (a.get(i) < b.get(i)){
-                bWin++;
-            }
-        }
-        List<Integer> result = new ArrayList<>();
-        result.add(aWin);
-        result.add(bWin);
-        return result;
-    }
-
-}
 public class CompareTheTriplets {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                .map(Integer::parseInt)
-                .collect(toList());
-
-        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                .map(Integer::parseInt)
-                .collect(toList());
-
-        List<Integer> result = Result.compareTriplets(a, b);
-
-        bufferedWriter.write(
-                result.stream()
-                        .map(Object::toString)
-                        .collect(joining(" "))
-                        + "\n"
-        );
-
-        bufferedReader.close();
-        bufferedWriter.close();
+        //내가 실질적으로 짠코드
+//        public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+//            // Write your code here
+//            int aWin = 0;
+//            int bWin = 0;
+//            for (int i = 0; i < a.size(); i++) {
+//                if (a.get(i) > b.get(i)) {
+//                    aWin++;
+//                }else if (a.get(i) < b.get(i)){
+//                    bWin++;
+//                }
+//            }
+//            List<Integer> result = new ArrayList<>();
+//            result.add(aWin);
+//            result.add(bWin);
+//            return result;
+//        }
     }
 }
 
