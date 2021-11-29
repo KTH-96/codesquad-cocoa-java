ff5f81d0781cd2b5ff5c6475c672d5e56c1f5915
KTH-96 <taehyun960@gmail.com>
2021-11-29 11:02:48 +0900

    step1 단어 입력및 체크

--- personal_project/src/main/java/step1/Step1.java
+++ personal_project/src/main/java/step1/Step1.java
@@ -2,10 +2,8 @@ package step1;
 
 public class Step1 {
     public static void main(String[] args) {
-        new Step1().run();
+        WordService wordService = new WordService();
+        wordService.run();
     }
 
-    private void run() {
-
-    }
 }
---
+++ personal_project/src/main/java/step1/WordService.java
@@ -0,0 +1,39 @@
+package step1;
+
+import java.util.Scanner;
+
+public class WordService {
+    private Scanner sc = new Scanner(System.in);
+    private Word wordEdit;
+
+    public void run() {
+        getWord();
+
+    }
+
+    //todo: 단어 입력하기
+    private void getWord() {
+        System.out.println("단어 이동수 방향 ex) apple 3 L");
+        System.out.print("> ");
+        String word = sc.next();
+        int moveNum = sc.nextInt();
+        String LR = sc.next();
+        LR.toLowerCase();
+
+        wordCheck(word, moveNum, LR);
+
+        wordEdit = new Word(word, moveNum, LR);
+    }
+    //todo: 단어 조건에 맞는지 체크하기
+    private void wordCheck(String word, int moveNum, String LR) {
+        if (word.split(" ").length >= 1){
+            System.out.println("띄워쓰기 들어가면 인식 못합니다 ㅠㅠ");
+        }
+        if (moveNum >= 100 || moveNum < -100) {
+            System.out.println("범위는 100 이하 -100 이하입니당");
+        }
+        if (!LR.equals("l") || !LR.equals("r")) {
+            System.out.println("L또는 R만 인식 가능합니다르");
+        }
+    }
+}
