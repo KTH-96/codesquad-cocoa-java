e44500c5cbedb44c2c8bf2a8931461e4dfa4733f
KTH-96 <taehyun960@gmail.com>
2021-11-29 11:21:34 +0900

    valid 추가

--- personal_project/src/main/java/step1/WordService.java
+++ personal_project/src/main/java/step1/WordService.java
@@ -20,20 +20,27 @@ public class WordService {
         String LR = sc.next();
         LR.toLowerCase();
 
-        wordCheck(word, moveNum, LR);
+        boolean valid = wordCheck(word, moveNum, LR);
+        while (valid){
+            getWord();
+        }
 
         wordEdit = new Word(word, moveNum, LR);
     }
-    //todo: 단어 조건에 맞는지 체크하기
-    private void wordCheck(String word, int moveNum, String LR) {
+    //todo: 단어 조건에 맞는지 체크하
+    private boolean wordCheck(String word, int moveNum, String LR) {
         if (word.split(" ").length >= 1){
             System.out.println("띄워쓰기 들어가면 인식 못합니다 ㅠㅠ");
+            return true;
         }
         if (moveNum >= 100 || moveNum < -100) {
             System.out.println("범위는 100 이하 -100 이하입니당");
+            return true;
         }
         if (!LR.equals("l") || !LR.equals("r")) {
             System.out.println("L또는 R만 인식 가능합니다르");
+            return true;
         }
+        return false;
     }
 }
