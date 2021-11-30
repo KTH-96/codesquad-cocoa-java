5b2dd282fb6c4405acfd5fb9a651b737917687b9
KTH-96 <taehyun960@gmail.com>
2021-11-30 16:26:00 +0900

    refactor: 단어뒤에 백쿼트를  붙였을때 인식못하는 부분 수정

--- personal_project/src/main/java/step2/Cube.java
+++ personal_project/src/main/java/step2/Cube.java
@@ -3,6 +3,8 @@ package step2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.StringTokenizer;
 
@@ -30,20 +32,41 @@ public class Cube {
     private boolean programStart() {
         System.out.print("CUBE> ");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String commands = "";
+        String command = "";
         try {
-            commands = br.readLine();
+            command = br.readLine();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        StringTokenizer st = new StringTokenizer(commands.toLowerCase(), "");
-
-        while (st.hasMoreTokens()) {
-            String commandWord = st.nextToken();
-            switch (commandWord) {
-
+        String[] splitCommand = command.split("");
+        //a a a ` a ` a a
+        List<String> commandArr = new ArrayList<>();
+        for (int i = 0; i < command.length(); i++) {
+            String commandWord = splitCommand[i];
+            if (commandWord.equals("`")) {
+                splitCommand[i - 1] += commandWord;
             }
         }
+        for (int i = 0; i < splitCommand.length; i++) {
+            if (splitCommand[i].equals("`")){
+                continue;
+            }
+            commandArr.add(splitCommand[i]);
+        }
+        for (String s : commandArr) {
+            System.out.println(s);
+        }
+
+
+//        switch (commandWord) {
+//            case "u":
+//                moveTopLeft();
+//            case "u`":
+//                moveTopRight();
+//            case "q":
+//                return false;
+//            }
+
         return false;
     }
 }
