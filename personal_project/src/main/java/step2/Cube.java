87e4e184355b1e583e03a10a437b55a38ef65f06
KTH-96 <taehyun960@gmail.com>
2021-11-30 19:05:35 +0900

    todo 리스트 작성

--- personal_project/src/main/java/step2/Cube.java
+++ personal_project/src/main/java/step2/Cube.java
@@ -5,17 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.StringTokenizer;
 
 public class Cube {
     String[][] cube = new String[3][3];
+
     public boolean Start() {
         firstCube();
 
         for (String[] strings : cube) {
             for (String string : strings) {
-                System.out.print(string+ " ");
+                System.out.print(string + " ");
             }
             System.out.println();
         }
@@ -23,9 +22,9 @@ public class Cube {
     }
 
     private void firstCube() {
-        String[][] firstCube = {{"R","R","W"},
-                        {"G","C","W"},
-                        {"G","B","B"}};
+        String[][] firstCube = {{"R", "R", "W"},
+                {"G", "C", "W"},
+                {"G", "B", "B"}};
         cube = firstCube;
     }
 
@@ -34,12 +33,12 @@ public class Cube {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String command = "";
         try {
-            command = br.readLine();
+            command = br.readLine().toLowerCase();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         String[] splitCommand = command.split("");
-        //a a a ` a ` a a
         List<String> commandArr = new ArrayList<>();
         for (int i = 0; i < command.length(); i++) {
             String commandWord = splitCommand[i];
@@ -48,25 +47,73 @@ public class Cube {
             }
         }
         for (int i = 0; i < splitCommand.length; i++) {
-            if (splitCommand[i].equals("`")){
+            if (splitCommand[i].equals("`")) {
                 continue;
             }
             commandArr.add(splitCommand[i]);
         }
-        for (String s : commandArr) {
-            System.out.println(s);
+
+        for (int i = 0; i < commandArr.size(); i++) {
+            switch (commandArr.get(i)) {
+                case "u":
+                    moveTopLeft();
+                    break;
+                case "u`":
+                    moveTopRight();
+                    break;
+                case "r":
+                    moveRightTop();
+                    break;
+                case "r`":
+                    moveRightBottom();
+                    break;
+                case "l":
+                    moveLeftBottom();
+                    break;
+                case "l`":
+                    moveLeftTop();
+                    break;
+                case "b":
+                    moveBottomRight();
+                    break;
+                case "b`":
+                    moveBottomLeft();
+                    break;
+                case "q":
+                    System.out.println("Bye~");
+                    return false;
+            }
         }
+        return true;
+    }
+    //todo: 가장 아래줄을 왼쪽으로 한칸 밀기
+    private void moveBottomLeft() {
+    }
+    //todo: 가장 아래줄을 오른쪽으로 한칸 밀기
+    private void moveBottomRight() {
+    }
+    //todo: 가장 왼쪽줄을 위로 한칸 밀기
+    private void moveLeftTop() {
+    }
+    //todo:가장 왼쪽줄을 아래로 한칸 밀기
+    private void moveLeftBottom() {
+    }
+    //todo: 가장 오른쪽줄을 아래로 한칸 밀기
+    private void moveRightBottom() {
+    }
+    //todo: 가장 오른쪽줄을 위로 한칸 밀기
+    private void moveRightTop() {
+    }
+    //todo: 가장 윗줄을 오른쪽으로 한칸 밀기
+    private void moveTopRight() {
+    }
+    //todo: 가장 윗줄을 왼쪽으로 한 칸 밀기
+    private void moveTopLeft() {
 
+    }
 
-//        switch (commandWord) {
-//            case "u":
-//                moveTopLeft();
-//            case "u`":
-//                moveTopRight();
-//            case "q":
-//                return false;
-//            }
-
-        return false;
+    private String[][] copyCube(){
+        String[][] dummyCube = cube;
+        return dummyCube;
     }
 }
